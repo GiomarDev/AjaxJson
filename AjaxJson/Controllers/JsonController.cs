09c2b8d33349bce4b4d2184fbef0d2adda8efee5
i201820189 <giomarlimocaldas1234@gmail.com>
2021-07-26 17:39:35 -0500

    cambios

---
+++ AjaxJson/Controllers/JsonController.cs
@@ -0,0 +1,37 @@
+﻿using AjaxJson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxJson.Controllers
+{
+    public class JsonController : Controller
+    {
+        // GET: Json
+        public ActionResult Index()
+        {
+            /*List<tblTable> lst;
+            using (AjaxJsonEntities db= new AjaxJsonEntities()) {
+                  lst = (from d in db.tblTable
+                           select d).ToList();
+            }*/
+            //ViewBag.Ngon.lst = lst;
+            return View(/*lst*/);
+        }
+
+        public JsonResult ObtenerPersonas() 
+        {
+            List<tblTable> lst;
+            using (AjaxJsonEntities db = new AjaxJsonEntities())
+            {
+                lst = (from d in db.tblTable
+                       select d).ToList();
+            }
+
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
